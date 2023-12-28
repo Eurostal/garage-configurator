a8ca809082c006627743963e6a04c65859394538
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-12-28 14:35:21 +0100

    Update translations

--- garage_woocommerce.js
+++ garage_woocommerce.js
@@ -1,6 +1,22 @@
 const { __, _x, _n, _nx } = wp.i18n;
 
-__( '__', 'my-domain' );
-_x( '_x', '_x_context', 'my-domain' );
-_n( '_n_single', '_n_plural', number, 'my-domain' );
-_nx( '_nx_single', '_nx_plural', number, '_nx_context', 'my-domain' );
+console.log(
+  __(
+    "There is no entrance to the garage, add a gate or door",
+    "garage-woocommerce"
+  )
+);
+
+__("Door height reduced", "garage-woocommerce");
+
+__(
+  "The garage cannot be changed, move or delete accessories on the walls.",
+  "garage-woocommerce"
+);
+
+__(
+  "You cannot change the type of roof, move or delete accessories on the walls.",
+  "garage-woocommerce"
+);
+
+__("There is no space on the selected wall.", "garage-woocommerce");
