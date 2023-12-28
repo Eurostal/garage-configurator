58f08368cb6a71793681fbee4d1e152a00c5e85e
Kamil <k.medrala99@gmail.com>
2023-12-28 14:59:35 +0100

    pot file update

--- garage_woocommerce.js
+++ garage_woocommerce.js
@@ -1,22 +1,11 @@
 const { __, _x, _n, _nx } = wp.i18n;
 
-console.log(
-  __(
-    "There is no entrance to the garage, add a gate or door",
-    "garage-woocommerce"
-  )
-);
+console.log(__("There is no entrance to the garage, add a gate or door", "garage-woocommerce"));
 
 __("Door height reduced", "garage-woocommerce");
 
-__(
-  "The garage cannot be changed, move or delete accessories on the walls.",
-  "garage-woocommerce"
-);
+__("The garage cannot be changed, move or delete accessories on the walls.", "garage-woocommerce");
 
-__(
-  "You cannot change the type of roof, move or delete accessories on the walls.",
-  "garage-woocommerce"
-);
+__("You cannot change the type of roof, move or delete accessories on the walls.", "garage-woocommerce");
 
 __("There is no space on the selected wall.", "garage-woocommerce");
